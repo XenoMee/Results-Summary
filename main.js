68b6f059f2c4859f5962a3f2f38de56a8f380950
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-03 22:57:57 +0300

    generate summary items asynchronous

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 fetch('data.json')
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const summaryItems = data
+      .map((item, index) => {
+        return `
+        <li class="summary-item" data-item-type="accent-${index + 1}">
+        <div class="flex-group">
+          <img src="${item.icon}" alt="">
+          <h3 class="summary-item-title">${item.category}</h3>
+        </div>
+        <p class="summary-score"><span>${item.score}</span>/100</p>
+      </li>
+        `;
+      })
+      .join('');
+
+    document.querySelector('.summary-list').innerHTML = summaryItems;
+  });
